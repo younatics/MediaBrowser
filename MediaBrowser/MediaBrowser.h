e0b1dc20f2c4ded401d43177f86a4fbda0a701e0
Seungyoun Yi <younatics@gmail.com>
2017-09-06 10:04:59 +0900

    add demo project

---
+++ MediaBrowser/MediaBrowser.h
@@ -0,0 +1,19 @@
+//
+//  MediaBrowser.h
+//  MediaBrowser
+//
+//  Created by Seungyoun Yi on 2017. 9. 6..
+//  Copyright © 2017년 Seungyoun Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MediaBrowser.
+FOUNDATION_EXPORT double MediaBrowserVersionNumber;
+
+//! Project version string for MediaBrowser.
+FOUNDATION_EXPORT const unsigned char MediaBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaBrowser/PublicHeader.h>
+
+
---
+++ MediaBrowserDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaBrowserDemo
+//
+//  Created by Seungyoun Yi on 2017. 9. 6..
+//  Copyright © 2017년 Seungyoun Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
