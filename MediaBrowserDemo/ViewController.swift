faf121f87f6df87c035a7f8bbf4092129a9a241a
Seungyoun Yi <younatics@gmail.com>
2017-09-06 14:16:21 +0900

    add examples

--- MediaBrowserDemo/ViewController.swift
+++ MediaBrowserDemo/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UITableViewController {
-
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
+    var selections = NSMutableArray()
+    var photos = NSMutableArray()
+    var thumbs = NSMutableArray()
+    var assets = NSMutableArray()
+//    var ALAssetsLibrary = ALAssetsLibrary()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let font = UIFont.systemFont(ofSize: 12)
+        segmentedControl.setTitleTextAttributes([NSFontAttributeName: font],
+                                                for: .normal)
+
+        loadAssets()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,42 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadAssets() {
+        let status = PHAuthorizationStatus.authorized
+        if status == .notDetermined {
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                if status == PHAuthorizationStatus.authorized {
+                    self.performLoadAssets()
+                }
+            })
+        } else if status == .authorized {
+            self.performLoadAssets()
+        }
+    }
+//    PHFetchOptions *options = [PHFetchOptions new];
+//    options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
+//    PHFetchResult *fetchResults = [PHAsset fetchAssetsWithOptions:options];
+//    [fetchResults enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+//    [_assets addObject:obj];
+//    }];
+//    if (fetchResults.count > 0) {
+//    [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
+//    }
+
+    func performLoadAssets() {
+        DispatchQueue.global(qos: .default).async {
+            let options = PHFetchOptions.init()
+            options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+            
+            let fetchResults = PHAsset.fetchAssets(with: options)
+            fetchResults.enumerateObjects({ (obj, idx, stop) in
+                self.assets.add(obj)
+            })
+            if fetchResults.count > 0 {
+                self.tableView.performSelector(onMainThread: #selector(self.tableView.reloadData), with: nil, waitUntilDone: false)
+            }
+        }
+    }
 
 }
 
