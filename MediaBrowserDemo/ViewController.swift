e3156f28504b39ade8992c82f970bc97cd293fe6
Seungyoun Yi <younatics@gmail.com>
2017-09-06 14:34:24 +0900

    add tableview data source

--- MediaBrowserDemo/ViewController.swift
+++ MediaBrowserDemo/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 import Photos
 
+class MediaBrowserCell: UITableViewCell {
+    
+}
+
 class ViewController: UITableViewController {
     @IBOutlet var segmentedControl: UISegmentedControl!
     
@@ -21,6 +25,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.tableView.register(MediaBrowserCell.self, forCellReuseIdentifier: "Cell")
         let font = UIFont.systemFont(ofSize: 12)
         segmentedControl.setTitleTextAttributes([NSFontAttributeName: font],
                                                 for: .normal)
@@ -31,6 +36,18 @@ class ViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return .none
     }
 
     func loadAssets() {
@@ -45,15 +62,6 @@ class ViewController: UITableViewController {
             self.performLoadAssets()
         }
     }
-//    PHFetchOptions *options = [PHFetchOptions new];
-//    options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
-//    PHFetchResult *fetchResults = [PHAsset fetchAssetsWithOptions:options];
-//    [fetchResults enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-//    [_assets addObject:obj];
-//    }];
-//    if (fetchResults.count > 0) {
-//    [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
-//    }
 
     func performLoadAssets() {
         DispatchQueue.global(qos: .default).async {
@@ -69,6 +77,78 @@ class ViewController: UITableViewController {
             }
         }
     }
+}
 
+//MARK: UITableViewDelegate, UITableviewDataSource
+extension ViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return assets.count > 0 ? 9 : 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellCheck = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        guard let cell = cellCheck else { return UITableViewCell() }
+        cell.accessoryType = segmentedControl.selectedSegmentIndex == 0 ? .disclosureIndicator : .none
+
+        switch (indexPath.row) {
+        case 0:
+            cell.textLabel?.text = "Single photo"
+            cell.detailTextLabel?.text = "with caption, no grid button"
+            break
+        case 1:
+            cell.textLabel?.text = "Multiple photos and video"
+            cell.detailTextLabel?.text = "with captions"
+            break
+            
+        case 2:
+            cell.textLabel?.text = "Multiple photo grid"
+            cell.detailTextLabel?.text = "showing grid first, nav arrows enabled"
+            break
+            
+        case 3:
+            cell.textLabel?.text = "Photo selections"
+            cell.detailTextLabel?.text = "selection enabled"
+            break
+            
+        case 4:
+            cell.textLabel?.text = "Photo selection grid"
+            cell.detailTextLabel?.text = "selection enabled, start at grid"
+            break
+            
+        case 5:
+            cell.textLabel?.text = "Web photos"
+            cell.detailTextLabel?.text = "photos from web"
+            break
+            
+        case 6:
+            cell.textLabel?.text = "Web photo grid"
+            cell.detailTextLabel?.text = "showing grid first"
+            break
+            
+        case 7:
+            cell.textLabel?.text = "Single video"
+            cell.detailTextLabel?.text = "with auto-play"
+            break
+            
+        case 8:
+            cell.textLabel?.text = "Web videos"
+            cell.detailTextLabel?.text = "showing grid first"
+            break
+        
+        case 9:
+            cell.textLabel?.text = "Library photos and videos"
+            cell.detailTextLabel?.text = "media from device library"
+            break
+            
+        default: break
+        }
+        
+        return cell
+    }
 }
 
