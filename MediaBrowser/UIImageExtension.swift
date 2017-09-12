c0edaa4be622d6656dd70ecccf9faacf8145b450
Seungyoun Yi <younatics@gmail.com>
2017-09-12 11:38:58 +0900

    change docs

--- MediaBrowser/UIImageExtension.swift
+++ MediaBrowser/UIImageExtension.swift
@@ -10,7 +10,7 @@
 import Foundation
 
 public extension UIImage {
-    /// Get bundle Image or retuen nil when it is not exist
+    /// Get bundle Image or return nil when it is not exist
     class func imageForResourcePath(name: String, inBundle: Bundle) -> UIImage? {
         return UIImage(named: name, in: inBundle, compatibleWith: nil)
     }
