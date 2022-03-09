20b2706635f8e0563b827110933ec26c10ec5316
Bonobo23 <bonobommm@gmail.com>
2022-03-09 10:30:32 +0500

    update package file

--- Package.swift
+++ Package.swift
@@ -3,3 +3,26 @@ import PackageDescription
 let package = Package(
     name: "MediaBrowser"
 )
+
+
+let package = Package(
+    name: "MediaBrowser",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "MediaBrowser",
+            targets: ["MediaBrowser"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .target(
+            name: "MediaBrowser",
+            dependencies: [],
+            path: "MediaBrowser",
+        )
+    ]
+)
