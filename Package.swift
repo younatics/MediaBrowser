3ed61743209ed77c21f66dc26cc53b06a8d84b3d
Bonobo23 <bonobommm@gmail.com>
2022-03-09 12:34:25 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .target(
             name: "MediaBrowser",
-            dependencies: [],
+            dependencies: ["SDWebImage"],
             path: "MediaBrowser"
         )
     ]
